8672030332afa85f994e35c57658773154142896
Ifeanyi Obinelo <ifeanyiobinelo@outlook.com>
2024-02-04 01:59:57 -0500

    Setup draft layout

--- src/app/journeys/[journey_id]/[section_id]/page.js
+++ src/app/journeys/[journey_id]/[section_id]/page.js
@@ -1,9 +1,13 @@
 "use client"
 
 import { AiOutlineSend } from "react-icons/ai"
-import { Tooltip } from "@chakra-ui/react"
+import { GrFormNextLink } from "react-icons/gr"
+
+import { Stack, Radio, RadioGroup, Tooltip } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { getSectionRequest } from "../../../../../api/sections"
+
+import SectionHeading from "@/components/SectionHeading"
 
 
 export default function JourneySectionView({ params }) {
@@ -18,28 +22,10 @@ export default function JourneySectionView({ params }) {
 
   }, [])
 
-  const messages = [
-    {"role": "user", "content": "Hello!"},
-    {"role": "assistant", "content": "How can I help?"},
-  ]
-
   return (
-    <main className="w-screen h-screen flex flex-row">
-
-      <div className="w-full p-3 flex items-center justify-center">
-        { section && section.content }
-      </div>
-
-      <div className="w-full">
-
-        <div className="w-full p-3 bg-blue-50 whitespace-pre-wrap">
-          { messages.map((message, i) => <SectionChatMessage key={i} message={message} /> ) }
-        </div>
-
-        <ChatInputField />
-
-      </div>
-      
+    <main className="w-screen h-screen flex flex-row overflow-hidden overflow-clip">
+      <SectionTextAndEvaluation section={section} />
+      <SectionChatWindow section={section} />  
     </main>
   )
 }
@@ -47,30 +33,144 @@ export default function JourneySectionView({ params }) {
 
 function ChatInputField() {
   return (
-    <form className="bg-white px-5 py-5 rounded-t-xl shadow-lg text-center z-30">
-      <div className="flex items-center justify-center">
-        
-        <textarea resize="none" className="rounded border-black border" rows={1} padding="5" placeholder="Need help? Ask me!" id="query" maxLength={4000} />
+    <form className="">
+      
+      <div className="flex flex-col bg-white rounded-lg w-full h-full border-2 border-cyan-800/[.25] text-gray-500 focus-within:border-cyan-700/[.75] transition-colors duration-500">
 
-        <Tooltip hasArrow label="Send message">
-          <div className="flex flex-col">
-            <button type="submit">
-              <AiOutlineSend />
-            </button>
-            <span className="text-orange-500">Submit</span>
-          </div>
-        </Tooltip>
+        <textarea className="resize-none p-4 rounded-lg grow text-lg outline-0" id="sandbox-input" placeholder="Ask me for help." />
+        
+        <div className="flex justify-between p-3">
+          <Tooltip hasArrow label="Send a message">
+            <button>
+              <AiOutlineSend size={32} className="fill-cyan-700" />
+            </button> 
+          </Tooltip>
+        </div>
 
       </div>
+
     </form>
   )
 }
 
 
-function SectionChatMessage({ message }) {
+function SectionTextAndEvaluation({ section }) {
   return (
-    <div className={`w-full p-3 ${message.role === "assistant" ? 'bg-blue-50' : ''} whitespace-pre-wrap`}>
-      { message.content }
+    <div className="w-full p-5 flex flex-col gap-y-5 overflow-y-auto">
+
+      <div className="mb-2 mt-6">
+        <SectionHeading>
+          Read the text.
+        </SectionHeading>
+      </div>
+
+      { section && section.content }
+
+      <div className="my-2">
+        <SectionHeading>
+          Now test your knowledge.
+        </SectionHeading>
+      </div>
+
+      <MultipleChoiceQuestion />
+      <MultipleChoiceQuestion />
+      <MultipleChoiceQuestion />
+      <MultipleChoiceQuestion />
+      <MultipleChoiceQuestion />
+
+      <MarkSectionAsCompleteButton />
+
+    </div>
+  )
+}
+
+
+function SectionChatWindow({ section }) {
+
+  const [response, setResponse] = useState('')
+
+  return (
+    <div className="w-full h-full p-5 overflow-hidden">
+
+      <div className="mb-8 mt-6">
+        <SectionHeading>
+          Need help? Ask an AI.
+        </SectionHeading>
+      </div>
+      
+      <ChatInputField />
+
+      <div className="w-full mt-5 whitespace-pre-wrap">
+        { response }
+      </div>
+
+    </div>
+  )
+}
+
+
+function MultipleChoiceQuestion() {
+
+  const [selected, setSelected] = useState(null)
+
+  const question = {
+    "question": "What is the relationship between force and mass? ",
+    "choices": [
+        {"label": "A", "text": "Force is the product of mass and acceleration."},
+        {"label": "B", "text": "Force is the sum of mass and acceleration."},
+        {"label": "C", "text": "Force is the difference between mass and acceleration."},
+        {"label": "D", "text": "There is no relationship between force and mass."}
+    ],
+    "answer": "A",
+    "explanation": "Newton's second law states that F = m * a."
+  }
+
+  return (
+    <div className="mb-3">
+      
+      <h2 className="font-semibold mb-1">
+        { question.question }
+      </h2>
+
+      <RadioGroup onChange={setSelected} value={selected} className="mb-4">
+        <Stack direction='column'>
+          { 
+          question.choices.map(
+            choice => 
+              <Radio value={ choice.label }>{ choice.text } </Radio>
+            ) 
+          }
+        </Stack>
+      </RadioGroup>
+
+      { 
+        selected && 
+        <div className={`py-2 px-3 ${selected !== question.answer ? 'bg-red-100' : 'bg-green-100'} rounded-md`}>
+          <h2 className={`font-semibold ${selected !== question.answer ? 'text-red-700' : 'text-green-700'} `}>
+            {
+              selected === question.answer ? "Correct" : "Incorrect"
+            }
+          </h2>
+          <div>
+            { question.explanation }
+          </div>
+        </div>
+      }
+
+    </div>
+  )
+
+}
+
+
+function MarkSectionAsCompleteButton() {
+  return (
+    <div className="px-3 mb-5">
+      <button className="w-full h-16 max-w-full rounded-lg bg-blue-500 hover:bg-blue-700 flex items-center justify-center">
+        <div className="text-white text-center font-semibold text-lg flex items-center justify-center gap-x-3">
+          <GrFormNextLink /> Mark this section as complete.
+        </div>
+      </button>
     </div>
   )
 }
