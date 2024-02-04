8db66594dfb1e96ea148cb4750f85595d36b6621
Ifeanyi Obinelo <ifeanyiobinelo@outlook.com>
2024-02-03 22:45:52 -0500

    Redirect the index page to the journeys route

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,17 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
+
 export default function Home() {
+
+  const router = useRouter()
+
+  // Redirect the index page to the journey's route
+  useEffect(() => {
+    router.push('/journeys')
+  }, [])
+
   return (
     <main>
       
