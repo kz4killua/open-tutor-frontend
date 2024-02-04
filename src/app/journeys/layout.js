b3c703d5b3a38cc4bfa54346dec23eaaeae55b95
Ifeanyi Obinelo <ifeanyiobinelo@outlook.com>
2024-02-03 23:40:54 -0500

    Update journeys view

--- src/app/journeys/layout.js
+++ src/app/journeys/layout.js
@@ -8,7 +8,7 @@ import LoginRequired from "@/components/LoginRequired"
 export default function JourneysLayout({ children }) {
   return (
     <LoginRequired>
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen bg-blue-50/[.50]">
         <Header />
         <div className="w-full">
           { children }
--- src/app/journeys/page.js
+++ src/app/journeys/page.js
@@ -4,6 +4,8 @@
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { getJourneysRequest } from "../../../api/journeys"
+import { FaPlus } from "react-icons/fa6"
+import { IoBookSharp } from "react-icons/io5"
 
 
 export default function JourneysListPage() {
@@ -20,9 +22,21 @@ export default function JourneysListPage() {
   return (
     <main className="w-screen flex flex-col items-center justify-center">
 
-      <div className="w-full max-w-3xl p-5 gap-y-5 flex flex-col items-center justify-center">
+      <div className="w-full max-w-3xl px-5 py-10 grid grid-cols-2 gap-y-5 gap-x-3 justify-items-center">
+
+        <div className="col-span-2 mt-7">
+          <SectionHeading>
+            Learn something new...
+          </SectionHeading>
+        </div>
 
         <CreateJourneyButton />
+
+        <div className="col-span-2 mt-7">
+          <SectionHeading>
+            ... or pick up where you left off.
+          </SectionHeading>
+        </div>
 
         {
           journeys.map(
@@ -39,7 +53,10 @@ export default function JourneysListPage() {
 
 function JourneyLink({ journey }) {
   return (
-    <Link className="w-72 h-16 max-w-full rounded-lg bg-blue-500 flex items-center justify-center" href={`/journeys/${journey.id}`}>
+    <Link className="w-full h-16 p-2 max-w-full rounded-lg bg-blue-500 hover:bg-blue-700 flex items-center justify-center gap-x-2" href={`/journeys/${journey.id}`}>
+      
+      <IoBookSharp className="fill-white" />
+
       <div className="text-white text-center font-semibold text-lg">
         { journey.title }
       </div>
@@ -50,10 +67,19 @@ function JourneyLink({ journey }) {
 
 function CreateJourneyButton() {
   return (
-    <div className="w-72 h-16 max-w-full rounded-lg bg-blue-500 flex items-center justify-center">
-      <div className="text-white text-center font-semibold text-lg">
-        Create Journey
+    <button className="col-span-2 w-full h-16 max-w-full rounded-lg bg-blue-500 hover:bg-blue-700 flex items-center justify-center">
+      <div className="text-white text-center font-semibold text-lg flex items-center justify-center gap-x-3">
+        <FaPlus /> Create a new learning journey.
       </div>
-    </div>
+    </button>
+    )
+}
+
+
+function SectionHeading({ children }) {
+  return (
+    <h1 className="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">
+      { children }
+    </h1>
   )
 }
