3ea4599e8a4db547d0f56076b8b6ffc6799e128d
Ifeanyi Obinelo <ifeanyiobinelo@outlook.com>
2024-02-03 22:40:13 -0500

    Update signup views

--- src/app/accounts/signup/page.js
+++ src/app/accounts/signup/page.js
@@ -4,6 +4,7 @@ import { useToast } from "@chakra-ui/react"
 import { useRouter } from "next/navigation"
 import { signUpRequest } from "../../../../api/accounts"
 import { requestAccessTokenRequest } from "../../../../api/accounts"
+import AccountsForm from "@/components/AccountsForm"
 
 
 export default function SignInPage() {
@@ -67,49 +68,8 @@ export default function SignInPage() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST" onSubmit={handleSignUp}>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+        
+        <AccountsForm onSubmit={handleSignUp} />
 
         <p className="mt-10 text-center text-sm text-gray-500">
           
