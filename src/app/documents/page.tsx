877089f7b952f8ab8448baf2b2e264234db4d95e
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-05 22:35:38 -0400

    Load documents in the /documents layout

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -57,24 +57,6 @@ function DocumentsList() {
 
   const { documents, documentsDispatch } = useDocuments()
 
-  useEffect(() => {
-    getDocumentsList()
-    .then(response => {
-      if (response.status >= 300) {
-        throw "Oops. We couldn't fetch your documents. Try reloading the page."
-      }
-      return response;
-    })
-    .then(response => {
-      documentsDispatch({ 
-        type: "SET", documents: response.data
-      })
-    })
-    .catch(message => {
-      toast.error(message)
-    })
-  }, [])
-
   return (
     <Table>
       <TableCaption>
