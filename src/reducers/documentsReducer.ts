bf2001868af395a994f8dcdb153118227c6f36e5
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-24 15:26:05 -0400

    Update the reducers to remove a document

--- src/reducers/documentsReducer.ts
+++ src/reducers/documentsReducer.ts
@@ -4,6 +4,7 @@ import { Document } from "@/types"
 export type DocumentsAction = 
   | { type: "ADD", document: Document }
   | { type: "SET", documents: Document[] }
+  | { type: "REMOVE", id: number }
 
 
 export default function documentsReducer(documents: Document[], action: DocumentsAction) {
@@ -12,6 +13,10 @@ export default function documentsReducer(documents: Document[], action: Document
       return [...documents, action.document]
     case "SET":
       return action.documents
+    case "REMOVE":
+      return documents.filter(document => 
+        document.id !== action.id
+      )
     default:
       throw Error(`Unknown action type: ${action}`)
   }
