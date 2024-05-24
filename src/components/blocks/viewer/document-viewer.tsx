ad483c937d1ced05eeccd03aad7a695ba7d1df61
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-05-24 11:13:19 -0400

    Listen for text highlights within the document

--- src/components/blocks/viewer/document-viewer.tsx
+++ src/components/blocks/viewer/document-viewer.tsx
@@ -1,17 +1,20 @@
 "use client"
 
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
+import './document-viewer.modules.css';
+
 import { pdfjs, Document, Page } from 'react-pdf';
+import type { PDFDocumentProxy } from 'pdfjs-dist';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-import './document-viewer.modules.css';
+import { type DocumentSelection } from '@/types';
+import { type Dispatch, type SetStateAction } from "react"
 
-import type { PDFDocumentProxy } from 'pdfjs-dist';
+// This type represents a user uploaded document.
+import { type Document as DocumentType } from "@/types"
 
-// This is the type representing a user uploaded document.
-import { Document as DocumentType } from "@/types"
-import { toast } from 'sonner';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -25,16 +28,37 @@ const options = {
 
 
 export function DocumentViewer({
-  document
+  document, selection, setSelection
 } : {
-  document: DocumentType
+  document: DocumentType, 
+  selection: DocumentSelection | null,
+  setSelection: Dispatch<SetStateAction<DocumentSelection | null>>
 }) {
 
   const [numPages, setNumPages] = useState<number>()
   const [documentWidth, setDocumentWidth] = useState<number>()
 
+  const ref = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
     setDocumentWidth(window.innerWidth * 0.95)
+  }, [])
+
+  // Listen for text highlights
+  useEffect(() => {
+    const handleSelection = () => {
+      const selection = window.document.getSelection()
+      if (selection && selection.rangeCount > 0) {
+        setSelection({
+          text: selection.toString().trim(),
+          boundingClientRect: selection.getRangeAt(0).getBoundingClientRect()
+        })
+      }
+    }
+    ref.current?.addEventListener('mouseup', handleSelection);
+    return () => {
+      ref.current?.removeEventListener('mouseup', handleSelection)
+    }
   }, [])
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
@@ -46,7 +70,7 @@ export function DocumentViewer({
   }
 
   return (
-    <div className='rendered-pdf'>
+    <div className='rendered-pdf' ref={ref}>
       <div className="rendered-pdf-container">
         <div className="rendered-pdf-container-document">
           <Document file={document.file} onLoadSuccess={onDocumentLoadSuccess} onLoadError={onDocumentLoadError} options={options}>
