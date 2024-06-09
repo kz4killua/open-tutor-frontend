b85a5b22102d9075606403f531251291bead69c1
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-06-09 12:26:49 -0400

    Make the site header sticky.

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 import { Icon } from "@/components/shared/icon"
 import React from "react";
+import { useState, useEffect } from "react";
 
 
 export interface HeaderBreadcrumbLink {
@@ -17,8 +18,22 @@ export function Header({
 } : { 
   links: HeaderBreadcrumbLink[] 
 }) {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  // Add a shadow when the user scrolls
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 40)
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
   return (
-    <header className={`flex items-center py-5 px-5 h-[${HeaderHeight}px]`}>
+    <header className={`flex z-50 bg-white items-center sticky top-0 py-5 px-5 h-[${HeaderHeight}px] transition-shadow ${isScrolled ? 'shadow' : ''}`}>
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
