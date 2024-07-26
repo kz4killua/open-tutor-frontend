064774716601b8029cc88e54829249041ad6a163
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-07-26 02:28:33 -0400

    Modify intersection thresholds for PDF pages.

--- src/components/documents/document-viewer.tsx
+++ src/components/documents/document-viewer.tsx
@@ -135,7 +135,7 @@ function DocumentPage({
           setCurrentPage(pageNumber)
         }
       },
-      { threshold: 0.5 / zoomLevel }
+      { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5] }
     )
 
     const element = ref.current
