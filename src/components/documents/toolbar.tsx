16f96360d8f9823713960d63dba38acc2cd2132c
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-07-26 00:57:06 -0400

    Update the zoom toolbar.

--- src/components/documents/toolbar.tsx
+++ src/components/documents/toolbar.tsx
@@ -2,12 +2,13 @@ import { ZoomIn, ZoomOut } from "lucide-react";
 import { useZoomLevel } from "@/app/providers";
 import StyledTooltip from "@/components/shared/styled-tooltip";
 
+
 export default function Toolbar() {
 
   const { zoomLevel, setZoomLevel } = useZoomLevel();
 
   function handleZoomIn() {
-    setZoomLevel(Math.min(zoomLevel + 0.05, 1.0))
+    setZoomLevel(Math.min(zoomLevel + 0.05, 2.5))
   }
 
   function handleZoomOut() {
@@ -16,9 +17,9 @@ export default function Toolbar() {
 
   return (
     <div className="fixed z-50 bottom-5 left-0 right-0 w-fit mx-auto flex items-center justify-center">
-      <div className="flex divide-x bg-white border shadow-lg rounded-lg">
+      <div className="flex bg-white border shadow-lg rounded-lg overflow-hidden">
         <StyledTooltip text="Zoom in">
-          <button className="cursor-pointer p-4 hover:bg-gray-50 active:bg-gray-200" onClick={handleZoomIn}>
+          <button className="cursor-pointer p-4 hover:bg-gray-50 active:bg-gray-200 border-r" onClick={handleZoomIn}>
             <ZoomIn size={20} />
           </button>
         </StyledTooltip>
