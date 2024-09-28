93031af25117d75f95bfd64fba142ae9c72c847f
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-09-28 11:55:28 -0400

    Add a function to create a flashcard from text

--- src/services/flashcards.ts
+++ src/services/flashcards.ts
@@ -1,13 +1,21 @@
 import instance from "./base";
+import type { Document, Flashcard } from "@/types";
 
 
-export async function getFlashcardsList(documentId: number) {
+export async function getFlashcardsList(documentId: Document["id"]) {
   return await instance.get(`/documents/${documentId}/flashcards`)
 }
 
 
-export async function getEvaluationFeedback(documentId: number, correct: number[], wrong: number[]) {
+export async function getEvaluationFeedback(documentId: Document["id"], correct: Flashcard["id"][], wrong: Flashcard["id"][]) {
   return await instance.post(`/documents/${documentId}/flashcards/feedback`, {
     correct: correct, wrong: wrong
+  })
+}
+
+
+export async function createFlashcardsFromText(documentId: Document["id"], text: string) {
+  return await instance.post(`/documents/${documentId}/flashcards/from-text`, {
+    text: text
   })
 }
