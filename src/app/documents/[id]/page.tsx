063f98773602a6eb416d89095b79c42abfd825a2
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-09-28 10:55:54 -0400

    Display the document overview on the detail page

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { Header } from "@/components/shared/header";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { BookOpen, Download, GraduationCap, Pencil, Trash } from "lucide-react";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { HeaderBreadcrumbLink } from "@/components/shared/header";
@@ -10,9 +11,11 @@ import { formatDate } from "@/utilities/dates";
 import Link from "next/link";
 import { formatFileSize } from "@/utilities/files";
 import { type Document } from "@/types";
-import { deleteDocument } from "@/services/documents";
+import { deleteDocument, getDocumentOverview } from "@/services/documents";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { Markdown } from "@/components/documents/markdown";
+import { useEffect, useState } from "react";
 
 
 export default function DocumentPage({ 
@@ -60,26 +63,11 @@ export default function DocumentPage({
               </Button>
             </Link>
           </div>
+          <div>
+            <DocumentOverview document={document} />
+          </div>
           <div className="flex flex-col md:flex-row gap-4">
-            <div className="grow bg-secondary rounded-lg p-4">
-              <h1 className="font-medium mb-4">
-                Document Details
-              </h1>
-              <div className="flex flex-col gap-y-2 text-sm">
-                <div>
-                  <h3 className="font-medium">Title</h3>
-                  <div>{document.name}</div>
-                </div>
-                <div>
-                  <h3 className="font-medium">Created</h3>
-                  <div>{formatDate(document.created)}</div>
-                </div>
-                <div>
-                  <h3 className="font-medium">File Size</h3>
-                  <div>{formatFileSize(document.size)}</div>
-                </div>
-              </div>
-            </div>
+            <DocumentDetails document={document} />
             <div className="flex flex-row justify-center md:justify-start md:flex-col gap-3 divide-y">
               <Link href={document.file} target="_blank">
                 <Button variant={"outline"} className="w-32">
@@ -92,6 +80,72 @@ export default function DocumentPage({
           </div>
         </main>
       }
+    </div>
+  )
+}
+
+
+function DocumentOverview({ document }: { document: Document }) {
+
+  const [overview, setOverview] = useState<string>()
+
+  useEffect(() => {
+    getDocumentOverview(document.id)
+    .then(response => {
+      if (response.status >= 300) {
+        throw "Oops. We couldn't fetch the overview. Try again."
+      }
+      return response
+    })
+    .then(response => {
+      setOverview(response.data.overview)
+    })
+  }, [])
+
+  return (
+    <div className="border shadow rounded-lg p-4">
+      <h1 className="font-medium mb-4">
+        Overview
+      </h1>
+      {
+        overview ? 
+          <Markdown>
+            {overview}
+          </Markdown>
+        :
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-full" />
+          </div>
+      }
+    </div>
+  )
+}
+
+
+function DocumentDetails({ document }: { document: Document }) {
+  return (
+    <div className="grow border shadow rounded-lg p-4">
+      <h1 className="font-medium mb-4">
+        Details
+      </h1>
+      <div className="flex flex-col gap-y-2 text-sm">
+        <div>
+          <h3 className="font-medium">Title</h3>
+          <div>{document.name}</div>
+        </div>
+        <div>
+          <h3 className="font-medium">Created</h3>
+          <div>{formatDate(document.created)}</div>
+        </div>
+        <div>
+          <h3 className="font-medium">File Size</h3>
+          <div>{formatFileSize(document.size)}</div>
+        </div>
+      </div>
     </div>
   )
 }
